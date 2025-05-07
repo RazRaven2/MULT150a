2ea08890f8cb8ae0a8ecb650235aba8bfd4e4a4b
RazRaven <156882612+RazRaven2@users.noreply.github.com>
2025-05-07 14:10:40 -0700

    Hour14_16 finished

---
+++ rdelacerda_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,8 @@
+/*
+Changes:
+1. background
+2. Font
+3. Start Button sprite
+4. Text color
+5. Added game description.
+*/
---
+++ rdelacerda_Hour14and16/Assets/rdelacerda_Hour16/ParticleEffect.cs
@@ -0,0 +1,8 @@
+/*
+Changes:
+1.  Added Rotation Speed at 90 angular velocity
+2.  Changed color to purple
+3.  Set shape to donut
+4.  Set start size to curve with 4 and a midpoint key at zero
+5.  Added noise
+*/
