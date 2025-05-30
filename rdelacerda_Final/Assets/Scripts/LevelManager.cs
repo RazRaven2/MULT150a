83e3c51328fb52c7237f0c0cd96809de97df74b7
RazRaven <156882612+RazRaven2@users.noreply.github.com>
2025-05-30 08:25:53 -0700

    Flying donut intro added
    All hail the Donut

--- rdelacerda_Final/Assets/Scripts/LevelManager.cs
+++ rdelacerda_Final/Assets/Scripts/LevelManager.cs
@@ -1,13 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
 
 public class LevelManager : MonoBehaviour
 {
 	public string sceneToLoad = "Game";
+public GameObject targetObject;
+	public Collidable scriptToEnable;
+	
 
 
-	public void LoadGame ()
+
+	public void LoadStart()
 	{
+		StartCoroutine(LoadGame());
+		EnableCollidableScript();
+	}
+	public IEnumerator LoadGame()
+	{
+		
+		yield return new WaitForSeconds(2.5f);
 		SceneManager.LoadScene(sceneToLoad);
 	}
+	public void EnableCollidableScript()
+    {
+        if (targetObject != null)
+        {
+            scriptToEnable = targetObject.GetComponent<Collidable>();
+            if (scriptToEnable != null)
+                scriptToEnable.enabled = true; // Enable the Collidable script
+        }
+
+    }
+
+
+
 }
